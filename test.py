81ed5f4c62d2ee22fba92c5da8cef837abacdea0
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2012-02-29 20:01:14 +0100

    WIP

--- test.py
+++ test.py
@@ -4,20 +4,17 @@ from pcre import *
 from pprint import pprint
 
 print pcre_version()
-options = 0 | PCRE_CASELESS
-pattern = r'(?<bob>f)(.)(?<jim>o)'
-#pattern = r'(xfo)o'
-print 'pattern = "%s"' % pattern
-compiled = pcre_compile(pattern, options)
-#pprint(compiled)
-
-subject = 'barfoObazfoo'
-print 'subject = "%s"' % subject
-
-extra = pcre_study(compiled)
-result = pcre_exec(compiled, subject, extra=extra)
-print '%d matches:' % result.num_matches
-for i in xrange(result.num_matches):
-    print ' "%s"' % result.matches[i]
-pprint(result.named_matches)
+for pattern, subject, options in [
+    [r'(?<bob>f)(.)(?<jim>o)', 'barfoObazfoo', 0 | PCRE_CASELESS],
+    ['abcd\t\n\r\x0c\x07\\e9;\\$\\\\?caxyz', 'abcd\t\n\r\x0c\x07\\e9;\\$\\?caxyz', 0],
+]:
+    print 'pattern = "%s"' % pattern
+    compiled = pcre_compile(pattern, options)
+    print 'subject = "%s"' % subject
+    extra = pcre_study(compiled)
+    result = pcre_exec(compiled, subject, extra=extra)
+    print '%d matches:' % result.num_matches
+    for i in xrange(result.num_matches):
+        print ' "%s"' % result.matches[i]
+    pprint(result.named_matches)
 
--- testsuite.py
+++ testsuite.py
@@ -3,9 +3,11 @@
 from pcre import *
 from pprint import pprint
 import sys
+import re
 
 OPTIONS = {
     'i': PCRE_CASELESS,
+    'x': PCRE_EXTENDED,
 }
 
 def test_match(regex, opts, data):
@@ -18,14 +20,30 @@ def test_match(regex, opts, data):
     result = pcre_exec(compiled, data, extra=extra)
     return result
 
+def process_regex(regex):
+    # handle C style escapes
+    regex = eval('"""%s"""' % regex)
+    return regex
+
+def process_data(data):
+    # escape unescaped '"'
+    data = re.sub('[^\\\\]\("\)', '\\"', data)
+    # eval
+    data = eval('"""%s"""' % data)
+    data = data.strip()
+    return data
+
 def main(*args):
     in_file = open(args[0])
     out_file = open(args[1], 'w')
     is_comment = False
     is_data = False
     regex = ''
+    multiline_regex = False
     opt = ''
+    line_no = 0
     for line in in_file:
+        line_no += 1
         if is_comment:
             # inside a comment
             if line[-4:] == '--/\n':
@@ -43,17 +61,35 @@ def main(*args):
             is_data = False
             out_file.write(line)
             continue
+        if multiline_regex:
+            regex_end = line.rfind('/')
+            if not(regex_end == -1 or line[regex_end - 1] == '\\'):
+                # last regex line
+                regex += line[1:regex_end]
+                regex = process_regex(regex)
+                opts = line[regex_end+1:-1]
+                multiline_regex = False
+            else:
+                regex += line
+            continue
         if line[0] == '/':
             # regex
             regex_end = line.rfind('/')
-            regex = line[1:regex_end].replace('\/', '/')
+            if regex_end in [-1, 0] or line[regex_end - 1] == '\\':
+                multiline_regex = True
+                regex = line[1:]
+                out_file.write(line)
+                continue
+            regex = process_regex(line[1:regex_end])
             opts = line[regex_end+1:-1]
             out_file.write(line)
             continue
         # it can be only data
         is_data = True
         out_file.write(line)
-        data = line.strip()
+        pprint([line_no, regex, opts, line])
+        data = process_data(line)
+        pprint([line_no, regex, opts, data])
         result = test_match(regex, opts, data)
         if result.num_matches:
             for i in xrange(result.num_matches):
